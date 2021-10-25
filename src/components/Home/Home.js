79d7e97cee89fe796b04494321ef1f16c14341be
Gokul-MDA <gokul.mda@gmail.com>
2021-10-25 15:51:48 +0530

    update about

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,37 +5,44 @@ import boy1 from "../../images/A_Boy.svg";
 import About from "../About/About";
 
 function Home() {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <Element name="hello">
-      <div className="home">
-        <div className="home__left">
-          <h3>
-            Hello there! <br /> I'm Gokul
-          </h3>
-          <h4>
-            Enthusiastic front-end developer, skilled in Javascript and ReactJs.
-            <b>
-              {" "}
-              Seeking opportunity to deliver high-quality Front-end products.
-            </b>{" "}
-            I can show my talent and enhance my skills to meet company goals.
-          </h4>
-          <button className="home__button" onClick={togglePopup}>
-            ABOUT ME
-          </button>
-          {isOpen && <About handleClose={togglePopup} />}
-        </div>
-        <div className="home__right">
-          <img src={boy1} alt="" />
-        </div>
-      </div>
-    </Element>
-  );
-}
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    };
+    return ( <
+        Element name = "hello" >
+        <
+        div className = "home" >
+        <
+        div className = "home__left" >
+        <
+        h3 >
+        Hello there! < br / > I 'm Gokul{" "} <
+        /h3>{" "} <
+        h4 >
+        Enthusiastic front - end developer, skilled in Javascript and ReactJs. { " " } <
+        b > { " " }
+        Seeking opportunity to deliver high - quality Front - end products. { " " } <
+        /b>{" "}
+        I can show my talent and enhance my skills to meet company goals. { " " } <
+        /h4>{" "} <
+        button className = "home__button"
+        onClick = { togglePopup } >
+        ABOUT ME { " " } <
+        /button>{" "} {
+            isOpen && < About handleClose = { togglePopup }
+            />}{" "} <
+            /div>{" "} <
+            div className = "home__right" >
+                <
+                img src = { boy1 }
+            alt = "" / >
+                <
+                /div>{" "} <
+                /div>{" "} <
+                /Element>
+        );
+    }
 
-export default Home;
+    export default Home;
